f6577352325a2d416522916f25e2dba4eed31e1c
anzhelikakuchma <anzhelika.kuchma@gmail.com>
2024-08-01 17:14:05 -0230

    unit tests for event service

---
+++ src/test/java/com/keyin/service/EventServiceUnitTest.java
@@ -0,0 +1,65 @@
+package com.keyin.service;
+
+import com.keyin.model.Event;
+import com.keyin.repository.EventRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProcedureServiceUnitTest {
+
+    EventService eventServiceUnderTest;
+
+    @Mock
+    EventRepository mockEventRepository;
+
+    @BeforeEach
+    void setup() {
+        this.eventServiceUnderTest = new EventService(this.mockEventRepository);
+    }
+
+    @Test
+    @DisplayName("Should return all events")
+    void findAllEvents() {
+        List<Event> eventListExpected = new ArrayList<>();
+
+        eventListExpected.add(new Event("first event"));
+        eventListExpected.add(new Event("second event"));
+
+        when(this.mockEventRepository.findAll()).thenReturn(eventListExpected);
+
+        List<Event> eventListActual = this.eventServiceUnderTest.findAllEvents();
+
+        assertAll(
+                () -> assertEquals(2, eventListActual.size()),
+                () -> assertTrue(eventListActual.stream().allMatch(procedure -> procedure.getName().contains("event")))
+        );
+    }
+
+    @Test
+    @DisplayName("Should return all events containing keyword")
+    void findEventByKeyword() {
+        List<Event> procedureListExpected = new ArrayList<>();
+
+        procedureListExpected.add(new Event("second event with test keyword"));
+
+        when(this.mockEventRepository.findByNameContainingIgnoreCase("test")).thenReturn(procedureListExpected);
+
+        List<Event> procedureListActual = this.eventServiceUnderTest.findEventByKeyword("test");
+
+        assertAll(
+                () -> assertEquals(1, procedureListActual.size()),
+                () -> assertEquals(procedureListExpected.getFirst(), procedureListActual.getFirst())
+        );
+    }
+}
